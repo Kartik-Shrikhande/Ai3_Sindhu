fcf630e8ee9dd3a5f85bb832581c388a9a41335d
KARTIK1111 <kartikshrikhande11@gmail.com>
2024-09-24 12:20:26 +0530

    testing 3

--- Ai3_Sindhu_Backend/src/index.js
+++ Ai3_Sindhu_Backend/src/index.js
@@ -2,7 +2,7 @@
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
-const router = require("./routes/routes")
+const router = require("../src/routes/routes")
 const cors = require('cors');
 require('dotenv').config({ path: '.env' })
 app.use(cors());
@@ -10,16 +10,12 @@ app.use(express.json())
 
 app.use('/', router);  // Make sure you point to the correct path
 
-// app.use('/',router)
-//connecting mongodb with nodejs
+
 mongoose.connect(process.env.MONGO_URL)
     .then(() => { console.log('MongoDB is connected !!') })
     .catch((error) => { console.log(`MONGODB ERROR:${error}`) })
 
     
-    app.get('/', (req, res) => {
-      res.send('Hello, World!');
-    });
 //creating server 
 app.listen(process.env.PORT, () => {
     console.log(`App is running on port ${process.env.PORT}`)
