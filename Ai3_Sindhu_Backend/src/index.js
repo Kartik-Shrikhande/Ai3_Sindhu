c591a5830ce77e953eb91a20922026d6880abdba
KARTIK1111 <kartikshrikhande11@gmail.com>
2024-09-24 12:05:01 +0530

    testing 1

--- Ai3_Sindhu_Backend/src/index.js
+++ Ai3_Sindhu_Backend/src/index.js
@@ -8,13 +8,16 @@ require('dotenv').config({ path: '.env' })
 app.use(cors());
 app.use(express.json())
 
-app.use('/',router)
+// app.use('/',router)
 //connecting mongodb with nodejs
 mongoose.connect(process.env.MONGO_URL)
     .then(() => { console.log('MongoDB is connected !!') })
     .catch((error) => { console.log(`MONGODB ERROR:${error}`) })
 
-
+    
+    app.get('/', (req, res) => {
+      res.send('Hello, World!');
+    });
 //creating server 
 app.listen(process.env.PORT, () => {
     console.log(`App is running on port ${process.env.PORT}`)
